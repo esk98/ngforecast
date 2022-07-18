4b60cf520eee67537999c61ccfb05788a70f57ec
Egor <eesk98@gmail.com>
2022-07-18 12:59:29 +0700

    eslint added

--- src/app/components/weathercard/weathercard.component.ts
+++ src/app/components/weathercard/weathercard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnChanges, SimpleChanges, Input, SimpleChange } from '@angular/core';
+import { Observable } from 'rxjs';
 import { WeatherService } from './weather.service';
 @Component({
   selector: 'app-weathercard',
@@ -7,14 +8,12 @@ import { WeatherService } from './weather.service';
 })
 export class WeathercardComponent implements OnInit, OnChanges {
   // to do: add eslint, remove logic from component to service
+  $weatherData!: Observable<any>;
   @Input() city!: string;
   constructor(public _getData: WeatherService) { }
   ngOnChanges(changes: SimpleChanges) {
     if(!changes['city'].isFirstChange())
-    this.getCityWeather(changes['city'].currentValue)
-  }
-  getCityWeather(city: string) {
-    this._getData.getWeather(city).subscribe(data => {  console.log(data) });
+    this.$weatherData = this._getData.getWeatherFromApi(changes['city'].currentValue)
   }
 
   ngOnInit() {

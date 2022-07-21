e423e96019f66adc95c4dbaa0d8274e72d13e857
esk98 <eesk98@gmail.com>
2022-07-21 16:09:20 +0700

    5 files modified, 1 deleted

--- src/app/shared/services/store.service.ts
+++ src/app/shared/services/store.service.ts
@@ -1,12 +1,46 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { 
+  shortWeather, 
+  dailyWeather, 
+  todayHighlights,
+  params
+} from '../models/models';
 @Injectable()
 export class StoreService {
 
   constructor() { }
-  city$;
-  shortWeather$;
-  dailyWeather$;
-  todayHighlights$;
-  params$
+
+  city$: BehaviorSubject<string> = new BehaviorSubject<string>('London');
+
+  shortWeather$: BehaviorSubject<shortWeather> = new BehaviorSubject<shortWeather>({
+    location: 'London',
+    icon: 'assets/a-cloudy.svg',
+    condition: 'Sunny',
+    temperature: 18,
+    wind: 2,
+    humidity: 3,
+    feelslike: 23,
+  });
+
+  dailyWeather$: BehaviorSubject<dailyWeather> = new BehaviorSubject<dailyWeather>({
+    data: [123012301, 10123123, 1231231, 12312312],
+    icon: ['assets/day.svg', 'assets/a-cloudy.svg', 'assets/day.svg', 'assets/a-cloudy.svg'],
+    temperature: [18, 23, 43, 53]
+  });
+
+  todayHighlights$: BehaviorSubject<todayHighlights> = new BehaviorSubject<todayHighlights>({
+    pressure: 18,
+    clouds: 19,
+    uvi: 2,
+    sunrise: 19,
+    sunset: 0.2,
+    visibility: 15,
+    dewPoint: 34
+  });
+  
+  params$: BehaviorSubject<params> = new BehaviorSubject<params>({
+    lat: 50,
+    lon: 33
+  });
 }

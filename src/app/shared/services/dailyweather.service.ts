595b87a62b26a7ee6b4df903bce63f5b5a62d0b3
esk98 <eesk98@gmail.com>
2022-07-21 18:28:31 +0700

    2 files modified

--- src/app/shared/services/dailyweather.service.ts
+++ src/app/shared/services/dailyweather.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { StoreService } from './store.service';
-
+import { params } from '../models/models';
 @Injectable({
   providedIn: 'root'
 })
 export class DailyweatherService {
 
-  constructor(public _store: StoreService, public _api: ApiService) { }
-  
-  getDailyWeather(lat: number, lon: number) { 
-    
+  constructor(public _store: StoreService, public _api: ApiService) { this.params = this._store.params$.getValue()}
+  params: params;
+  getDailyWeather() { 
+    console.log('test')
+    console.log(this.params.lat, this.params.lon )
   }
 
 }

76b81ca267415d2c3b9e460804615ca85cbd1bfe
esk98 <eesk98@gmail.com>
2022-07-21 15:37:18 +0700

    1 file added

---
+++ src/app/shared/models/models.ts
@@ -0,0 +1,26 @@
+export interface city {
+  city: string;
+},
+export interface shortWeather {
+  location: string;
+  icon: string;
+  condition: string;
+  temperature: number;
+  wind: number;
+  humidity: number;
+  feelslike: number;
+},
+export interface dailyWeather {
+  data: number;
+  icon: string;
+  temperature: number;
+},
+export interface todayHighlights {
+  pressure: number;
+  clouds: number;
+  uvi: number;
+  sunrise: number;
+  sunset: number;
+  visibility: number;
+  dewPoint: number;
+}

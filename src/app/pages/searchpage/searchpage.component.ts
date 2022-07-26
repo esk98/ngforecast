8bd0edd611052238fc7176a12d17d93d4b85fe57
esk98 <eesk98@gmail.com>
2022-07-26 19:28:59 +0700

    2 files modified

--- src/app/pages/searchpage/searchpage.component.ts
+++ src/app/pages/searchpage/searchpage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { shortWeather } from '../../shared/models/models';
 import { ShortWeatherService } from '../../shared/services/shortweather.service';
@@ -7,16 +7,16 @@ import { ShortWeatherService } from '../../shared/services/shortweather.service'
     templateUrl: './searchpage.component.html',
     styleUrls: ['./searchpage.component.scss'],
 })
-export class SearchpageComponent {
+export class SearchpageComponent implements OnInit{
     shortWeather$!: Observable<shortWeather>;
     city$: BehaviorSubject<string> = new BehaviorSubject<string>('London');
     constructor(public ShortWeatherService: ShortWeatherService) {}
 
+    ngOnInit(): void {
+        this.onSearch('new york');
+    }
     onSearch(city: string) {
-        if (!city) {
-            console.log('Input city')
-        } else {
-            this.shortWeather$ = this.ShortWeatherService.getShortWeather(city);
-        };
+        console.log(city)
+        this.shortWeather$ = this.ShortWeatherService.getShortWeather(this.city$.getValue());
     }
 }

4735e97bdc415b2e8d828aab6500453dbc69f9b3
esk98 <eesk98@gmail.com>
2022-07-27 18:30:42 +0700

    1 file modified

--- src/app/components/dailyweather/dailyweather.component.ts
+++ src/app/components/dailyweather/dailyweather.component.ts
@@ -2,11 +2,7 @@ import {
     ChangeDetectionStrategy,
     Component,
     Input,
-    OnInit,
 } from '@angular/core';
-import { DailyweatherService } from '../../shared/services/dailyweather.service';
-import { StoreService } from '../../shared/services/store.service';
-import { LocationService } from '../../shared/services/location.service';
 @Component({
     selector: 'app-dailyweather',
     templateUrl: './dailyweather.component.html',

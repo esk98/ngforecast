08e8f36c91e430731b7a058cd06eefaf4a8b0590
esk98 <eesk98@gmail.com>
2022-07-27 14:29:46 +0700

    2 files modified

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, filter, Observable } from 'rxjs';
 import { AutoComplete } from '../../shared/services/autocomplete.service';
 import { LocationService } from '../../shared/services/location.service';
@@ -12,7 +12,11 @@ import { DailyweatherService } from '../../shared/services/dailyweather.service'
 })
 export class SearchComponent implements OnInit {
     @Output() newSearchEvent = new EventEmitter<string>();
-    inputFormControl = new FormControl<string>('');
+    
+    searchForm = new FormGroup({
+        inputFormControl: new FormControl<string>('', [Validators.required]),
+    })
+        
     autoCompleteNames$!: Observable<string[]>;
     constructor(
         public _catchInputVal: AutoComplete,
@@ -20,12 +24,8 @@ export class SearchComponent implements OnInit {
         public _get: DailyweatherService
     ) {}
     ngOnInit() {
-        this.inputFormControl.valueChanges
+        this.searchForm.valueChanges
             .pipe(
-                filter(
-                    (value: string | null) =>
-                        value !== null && value?.length > 2
-                ),
                 distinctUntilChanged(),
                 debounceTime(500)
             )
